9faa092be71c271a05e81f21276812b12a533ecc
Ivan Kozhokar <dmb14suka@gmail.com>
2021-12-03 12:59:21 -0800

    add new div with list of new todo items with javascript

--- app.js
+++ app.js
@@ -5,6 +5,33 @@ const todoList = document.querySelector('.todo-list');
 
 
 //event listners
-
+todoButton.addEventListener('click', addTodo);
 
 //functions
+
+function addTodo(event){
+    //prevent form from submitting
+    event.preventDefault();
+    // todo div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    // create LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    // check mark button
+    const completedButton = document.createElement('button');
+    completedButton.classList.add('complete-btn');
+    completedButton.innerHTML = '<i class ="fas fa-check"></i>';
+    todoDiv.appendChild(completedButton);
+    // trash button
+    const trashButton = document.createElement('button');
+    trashButton.classList.add('trash-btn');
+    trashButton.innerHTML = '<i class = "fas fa-trash"></i>';
+    todoDiv.appendChild(trashButton);
+    // append tododiv to todolist
+    todoList.appendChild(todoDiv);
+}
+
+//original copy
